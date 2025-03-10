e94c521cc4d35643183ab53d652dad14defdc399
Melanie Meléndez <57066220+MelanieRD@users.noreply.github.com>
2025-03-10 02:18:22 -0400

    --allow-emptyflag

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: 'dist', // Asegura que Vite genera `dist` en `client`
+  },
   
 })
